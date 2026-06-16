234402ac3a159757f868e2770e4af756a8ad55e6
Mario Benjamin Prado Vargas <114129410+mariopvdev@users.noreply.github.com>
2026-06-16 13:40:13 -0500

    Fix/frontend local api ipv4 (#3)
    * fix(frontend): use IPv4 loopback for local API
    
    * Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5000',
+  apiBaseUrl: 'http://127.0.0.1:5000',
 };
+
+// Si no se quiere usar docker local, se puede usar la siguiente URL:
+// apiBaseUrl: 'http://localhost:5000'
