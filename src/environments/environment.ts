debd234c975e2a698be9e78e5e6360e2b008421a
Mariobpv22 <mariobenjamin003@gmail.com>
2026-06-16 13:39:31 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,6 @@ export const environment = {
   production: false,
   apiBaseUrl: 'http://127.0.0.1:5000',
 };
+
+// Si no se quiere usar docker local, se puede usar la siguiente URL:
+// apiBaseUrl: 'http://localhost:5000'
